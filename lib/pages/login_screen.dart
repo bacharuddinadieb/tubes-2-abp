09016396840eb730b97a1bad073b7a605df97610
Muhamad Andera Famuzia <60535684+AnderaFamuzia@users.noreply.github.com>
2022-06-21 10:27:04 +0700

    login

---
+++ lib/pages/login_screen.dart
@@ -0,0 +1,101 @@
+// import 'dart:convert';
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+// import '../Services/auth_services.dart';
+// import '../Services/globals.dart';
+import '../Services/globals.dart';
+import '../rounded_button.dart';
+import '../services/auth_service.dart';
+import 'home.dart';
+import 'package:http/http.dart' as http;
+
+// import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _email = '';
+  String _password = '';
+  loginPressed() async {
+    if (_email.isNotEmpty && _password.isNotEmpty) {
+      http.Response response = await AuthServices.login(_email, _password);
+      Map responseMap = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        // ignore: use_build_context_synchronously
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => const HomeScreen(),
+            ));
+      } else {
+        // ignore: use_build_context_synchronously
+        errorSnackBar(context, responseMap.values.first);
+      }
+    } else {
+      errorSnackBar(context, 'enter all required fields');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 32, 32, 228),
+          centerTitle: true,
+          elevation: 0,
+          title: const Text(
+            'Login',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Image.asset('assets/images/login.jpg'),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your password',
+                ),
+                onChanged: (value) {
+                  _password = value;
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              RoundedButton(
+                btnText: 'LOG IN',
+                onBtnPressed: () => loginPressed(),
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/pages/register_screen.dart
@@ -0,0 +1,137 @@
+import 'dart:convert';
+
+import 'package:abp_tubes_2/pages/home.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutterapp/Screens/login_screen.dart';
+
+// import '../Services/auth_services.dart';
+// import '../Services/globals.dart';
+import '../Services/globals.dart';
+import '../rounded_button.dart';
+import 'package:http/http.dart' as http;
+
+// import 'home_screen.dart';
+import '../services/auth_service.dart';
+import 'login_screen.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String _email = '';
+  String _password = '';
+  String _name = '';
+
+  createAccountPressed() async {
+    bool emailValid = RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(_email);
+    if (emailValid) {
+      http.Response response =
+          await AuthServices.register(_name, _email, _password);
+      Map responseMap = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        // ignore: use_build_context_synchronously
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => const HomeScreen(),
+            ));
+      } else {
+        // ignore: use_build_context_synchronously
+        errorSnackBar(context, responseMap.values.first[0]);
+      }
+    } else {
+      errorSnackBar(context, 'email not valid');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 32, 32, 228),
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          'Registration',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Name',
+              ),
+              onChanged: (value) {
+                _name = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            RoundedButton(
+              btnText: 'Create Account',
+              onBtnPressed: () => createAccountPressed(),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const LoginScreen(),
+                    ));
+              },
+              child: const Text(
+                'already have an account',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
