77da88ccdaf3b49f67de28c54b4ff6321f3ea5f8
Nur Wakhidah Fitri Amalia <nurwakhidahfitriamalia@gmail.com>
2022-06-26 21:32:51 +0700

    menambahkan peta dan memperbaiki home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home {
   final String id;
@@ -54,6 +54,21 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<Home> futureData;
+  static LatLng lokasi = const LatLng(-6.975575564736307, 107.63055772782457);
+  static CameraPosition cameraPosition = CameraPosition(
+    target: lokasi,
+    zoom: 18,
+  );
+
+  Set<Marker> markerGedung() {
+    return {
+      Marker(
+          markerId: const MarkerId('gedung'),
+          infoWindow: const InfoWindow(title: 'Gedung Marriot'),
+          position: lokasi
+      ),
+    };
+  }
 
   @override
   void initState() {
@@ -68,32 +83,64 @@ class _HomeScreenState extends State<HomeScreen> {
           future: futureData,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              Home? data = snapshot.data;
-              return Column(
-                children: [
-                  Image.network(data!.gambar),
-                  Container(
-                    margin: EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          data!.nama,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 24),
-                        ),
-                        Text(
-                          data!.alamat,
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 2),
-                          child: Text(data!.deskripsi),
-                        )
-                      ],
+              Home data = snapshot.data ??
+                  const Home(
+                      id: '',
+                      alamat: '',
+                      deskripsi: '',
+                      gambar: '',
+                      latitude: 0,
+                      longitude: 0,
+                      nama: '');
+              return SingleChildScrollView(
+                child: Column(
+                  children: [
+                    FadeInImage.assetNetwork(
+                      image: data.gambar,
+                      placeholder:
+                      'assets/images/Loading_icon.gif',
                     ),
-                  )
-                ],
+                    Container(
+                      margin: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data.nama,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w500, fontSize: 24),
+                          ),
+                          Text(
+                            data.alamat,
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 4),
+                            child: Text(data.deskripsi),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 16),
+                            child: const Text(
+                              'Lokasi',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500, fontSize: 18),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 4),
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            child: GoogleMap(
+                              initialCameraPosition: cameraPosition,
+                              mapType: MapType.normal,
+                              markers: markerGedung(),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               );
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
